972211981d7dd21b9535e751584f931b8146c66e
rogersilva9 <rogersilva9@gmail.com>
2021-08-12 17:51:22 -0300

    novo

---
+++ ex_proposto5.2.c
@@ -0,0 +1,20 @@
+/* Exercício Proposto - Aula 5.2*/
+
+/* Agosto de 2021 */
+
+/* Bibliotecas */
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <locale.h>
+
+/* Função Principal */
+
+int main()
+{
+	//setlocale(LC_ALL,"Portuguese_Brazil");
+
+	printf("teste de caracteres çãáàâüÉé\n");
+        return 0;
+}
+/* Fim da Função Principal */
