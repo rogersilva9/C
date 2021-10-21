68370c4c02ef3cf3fa91d9b137bebc5e3e22dd51
rogersilva9 <rogersilva9@gmail.com>
2021-10-21 17:32:14 -0300

    Create ex_proposto8.3.c

---
+++ ex_proposto8.3.c
@@ -0,0 +1,77 @@
+/* ============================================================================
+    Linguagem C, o curso definitivo
+    Módulo 8 Aula 3
+    Título da Aula
+    
+    Autor: Msc. Eng. Rogério Souza da Silva
+    Data:  Outubro de 2021
+
+    
+Exercício proposto: a função protocol apresentada no Box 7 implementa um protocolo
+que verifica dois operandos. Sendo o primeiro operando maior, a função retorna um ‘H’ de high,
+do contrário, retorna um ‘L’ de low.
+
+Implemente uma função que irá receber dois operandos inteiros e a função protocol
+como parâmetro, interpretando o protocolo corretamente. A função apresentará a seguinte
+saída:
+“Resultado do protocolo: H” para quando o primeiro operando for maior. Do contrário:
+“Resultado do protocolo: L”
+
+============================================================================ */
+
+
+/* ========================================================================= */
+/* --- Bibliotecas --- */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* ========================================================================= */
+/* --- Constantes --- */
+
+
+/* ========================================================================= */
+/* --- Protótipo das Funções --- */
+
+
+/* ========================================================================= */
+/* --- Variáveis Globais --- */
+
+
+/* ========================================================================= */
+/* --- Função Principal --- */
+int main()
+{
+      
+  
+  return 0;                                      /* retorna 0, se main for executada corretamente */
+  
+} /* end main */
+
+
+/* ========================================================================= */
+/* --- Desenvolvimento de funções --- */
+
+
+/* ============================================================================  
+                                                              
+                                       _                      
+                                      / \                     
+                                     |oo >                    
+                                     _\=/_                    
+                    ___         #   /  _  \   #               
+                   /<> \         \\//|/.\|\\//                
+                 _|_____|_        \/  \_/  \/                 
+                | | === | |          |\ /|                    
+                |_|  0  |_|          \_ _/                    
+                 ||  0  ||           | | |                    
+                 ||__*__||           | | |                    
+                |* \___/ *|          []|[]                    
+                /=\ /=\ /=\          | | |                    
+________________[_]_[_]_[_]_________/_]_[_\_______________________________
+                                                              
+                                                              
+============================================================================ */
+/* --- Final do Programa --- */
+
+
