ab1696baae2d7500cfed2d2463b4c74d619adf77
rogersilva9 <rogersilva9@gmail.com>
2021-12-15 15:23:36 -0300

    Create ex_proposto9.0.c

---
+++ ex_proposto9.0.c
@@ -0,0 +1,71 @@
+/* ============================================================================
+    Linguagem C, o curso definitivo
+    Módulo 9 Aula 0
+    Título da Aula
+    
+    Autor: Msc. Eng. Rogério Souza da Silva
+    Data:  Dezembro de 2021
+
+    
+Exercício proposto: Desenvolva o projeto de um programa para criptografar arquivos
+de texto, que desloque os caracteres digitados 3 posições para frente.
+Exemplo:
+Mensagem original: Curso de C
+Mensagem criptografada: Fxuvr#gh#F
+Desenvolva um software para remover a criptografia e gerar um novo arquivo com a
+mensagem original.
+Exemplo:
+Mensagem criptografada: Fxuvr#gh#F
+Criptografia removida: Curso de C
+
+============================================================================ */
+
+
+/* ========================================================================= */
+/* --- Bibliotecas --- */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* ========================================================================= */
+/* --- Constantes --- */
+
+
+/* ========================================================================= */
+/* --- Protótipo das Funções --- */
+
+
+/* ========================================================================= */
+/* --- Variáveis Globais --- */
+
+/* ========================================================================= */
+/* --- Função Principal --- */
+int main()
+{
+}
+/* --- Desenvolvimento de funções --- */
+
+
+
+/* ============================================================================  
+                                                              
+                                       _                      
+                                      / \                     
+                                     |oo >                    
+                                     _\=/_                    
+                    ___         #   /  _  \   #               
+                   /<> \         \\//|/.\|\\//                
+                 _|_____|_        \/  \_/  \/                 
+                | | === | |          |\ /|                    
+                |_|  0  |_|          \_ _/                    
+                 ||  0  ||           | | |                    
+                 ||__*__||           | | |                    
+                |* \___/ *|          []|[]                    
+                /=\ /=\ /=\          | | |                    
+________________[_]_[_]_[_]_________/_]_[_\_______________________________
+                                                              
+                                                              
+============================================================================ */
+/* --- Final do Programa --- */
+
+
